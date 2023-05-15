05a872d77c0342518638756fe2b3b393a4b3b0aa
rsedighi <ramin-og@MacBook-Pro-3.local>
2023-05-15 16:11:45 -0700

    first commit

---
+++ plugins/netlify-plugin-prod-notifications/index.js
@@ -0,0 +1,27 @@
+module.exports = {
+    async onSuccess() {
+      netlifyConfig.build.environment.WHICH_DEPLOY = getDeployStage();
+      console.log("onSuccess: I run on build success " + getDeployStage + " 🎉.");
+if (getDeployStage() === "production") {
+      const sgMail = require("@sendgrid/mail");
+      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+      const msg = {
+        to: process.env.EMAIL_TO, // Change to your recipient
+        from: process.env.APPROVED_SENDER, // Change to your verified sender
+        subject: process.env.EMAIL_SUBJECT,
+        text: process.env.DEPLOY_URL,
+      };
+      try {
+        await sgMail.send(msg);
+        console.log("Email sent");
+      } catch (error) {
+        console.error(error);
+  
+        if (error.response) {
+          console.error(error.response.body);
+        }
+      }
+      console.log(process.env.DEPLOY_URL);
+    },
+  }
+  };
