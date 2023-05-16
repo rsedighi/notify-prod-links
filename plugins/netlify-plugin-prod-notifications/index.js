b4426ed31c08f171bffae3f0295af65bb5677bd2
rsedighi <ramin-og@MacBook-Pro-3.local>
2023-05-16 16:24:24 -0700

    sendgrid example commented out

--- plugins/netlify-plugin-prod-notifications/index.js
+++ plugins/netlify-plugin-prod-notifications/index.js
@@ -2,6 +2,32 @@ module.exports = {
     async onSuccess() {
     console.log("onSuccess: I run on build success " + process.env.WHICH_DEPLOY + " 🎉.");
     console.log(process.env.DEPLOY_URL);
-    //  Other logic goes here. Can choose to send an email when deploy == production
+    
+  
+    //  Example code for sending an email with Sendgrid 
+    // Make sure to add the correct ENV names to your Netlify site settings
+
+  //   const sgMail = require("@sendgrid/mail");
+  //   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  //   const msg = {
+  //     to: process.env.EMAIL_TO, // Change to your recipient
+  //     from: process.env.APPROVED_SENDER, // Change to your verified sender
+  //     subject: process.env.EMAIL_SUBJECT,
+  //     text: process.env.DEPLOY_URL,
+  //   };
+  //   try {
+  //     await sgMail.send(msg);
+  //     console.log("Email sent");
+  //   } catch (error) {
+  //     console.error(error);
+
+  //     if (error.response) {
+  //       console.error(error.response.body);
+  //     }
+  //   }
+  //   console.log(process.env.DEPLOY_URL);
+  // },
+
+
   }
   };
