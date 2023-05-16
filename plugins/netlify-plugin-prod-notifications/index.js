b096d7d94a5d8e07f9814594cf513e10c81a5cdb
rsedighi <ramin-og@MacBook-Pro-3.local>
2023-05-16 13:15:43 -0700

    added netlifyConfig

--- plugins/netlify-plugin-prod-notifications/index.js
+++ plugins/netlify-plugin-prod-notifications/index.js
@@ -1,6 +1,7 @@
-module.exports = {
-    async onSuccess() {
+// module.exports = {
+    // async onSuccess() {
+      export const onSuccess = function ({netlifyConfig}){
       netlifyConfig.build.environment.WHICH_DEPLOY = getDeployStage();
       console.log("onSuccess: I run on build success " + getDeployStage + " 🎉.");
-    }
+    // }
   }
