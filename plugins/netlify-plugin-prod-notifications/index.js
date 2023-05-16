8c97917977b12b771a1b133e468d7622dd3895bc
rsedighi <ramin-og@MacBook-Pro-3.local>
2023-05-16 16:14:42 -0700

    cleaned up code

--- plugins/netlify-plugin-prod-notifications/index.js
+++ plugins/netlify-plugin-prod-notifications/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     async onSuccess() {
     console.log("onSuccess: I run on build success " + process.env.WHICH_DEPLOY + " 🎉.");
-  
+    console.log(process.env.DEPLOY_URL);
+    //  Other logic goes here. Can choose to send an email when deploy == production
   }
   };
